a0f716e5133070a2c69884e2a7dc1f25724ea3fb
CccC-maker <1572157545@qq.com>
2022-04-18 12:13:04 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/com/example/lnb/entity/Orders.java

---
+++ src/main/java/com/example/lnb/entity/dto/Users_DTO.java
@@ -0,0 +1,13 @@
+package com.example.lnb.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class Users_DTO {
+
+    private String msg;
+
+    private boolean judgeinfo;
+
+    private int account;
+}
---
+++ src/main/java/com/example/lnb/entity/dto/Workers_DTO.java
@@ -0,0 +1,16 @@
+package com.example.lnb.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class Workers_DTO {
+
+    private String msg;
+
+    private boolean judgeinfo;
+
+    private int waccount;
+
+    private int wscore;
+
+}
